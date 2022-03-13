81919d26816cfc6e417e623190d8fd0e35320311
lensilver <lensilverpr@hotmail.com>
2022-03-13 12:21:39 +0000

    Added model hashtag_question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,12 @@
 class Question < ApplicationRecord
-
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
-    
+
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
+
+  scope :answered, -> { where.not(answer: nil) }
+  scope :unanswered, -> { where(answer: nil) }
+
   validates :text, presence: true, length: { maximum: 255 }
 end
